c81f9f6c0dd4ecb494eddcfdce7505a9046a651d
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2022-06-07 19:47:02 -0300

    Create recuperar.js

---
+++ css/recuperar.js
@@ -0,0 +1,12 @@
+function pinicial(){
+  location.replace("index.html")
+}
+function logar(){
+  location.replace("logar.html")
+}
+function denuncia(){
+  location.replace("construcao.html")
+}
+function ajuda(){
+  location.replace("construcao.html")
+}

b2d47b169e539cb795d6c902d2067e4d7d272c9c
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2022-06-07 19:49:11 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function refugiado(){
+  location.replace("crefugiada.html")
+}
+function host(){
+  location.replace("chost.html")
+}
+function logar(){
+  location.replace("logar.html")
+}
+function pinicial(){
+  location.replace("index.html")
+}
+function denuncia(){
+  location.replace("construcao.html")
+}
+function ajuda(){
+  location.replace("construcao.html")
+}
